7ba9ba6b2cdfedb3f9c4f822d12ff4df622cb761
Sreejit-Sengupto <sreesen2003@gmail.com>
2024-02-04 13:47:43 +0530

    😩

--- src/components/Home/VideoSection.jsx
+++ src/components/Home/VideoSection.jsx
@@ -11,25 +11,30 @@ const VideoSection = () => {
           src="https://www.youtube.com/embed/MvEIKOSEcqo?si=4r1JKuonbLxACy30"
           title="Ravi fans"
           frameborder="0"
-        //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        //   allowfullscreen
+          //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          //   allowfullscreen
           className="rounded-2xl"
         ></iframe>
       </div>
       <div className="w-[50%] flex flex-col items-start justify-center">
         <div>
-            <h1 className="text-5xl font-bold max-w-[60%]">
+          <h1 className="text-5xl font-bold max-w-[60%]">
             Crafting Superior Fans for Modern Living
-            </h1>
+          </h1>
         </div>
         <div className="my-8">
-            <p className="max-w-[60%] text-[#6A6C78]">
-            Discover the art of innovation as we redefine cooling solutions. Elevate your surroundings with our cutting-edge fans, designed for efficiency, style, and unparalleled comfort. Join the future of airflow!
-            </p>
+          <p className="max-w-[60%] text-[#6A6C78]">
+            Discover the art of innovation as we redefine cooling solutions.
+            Elevate your surroundings with our cutting-edge fans, designed for
+            efficiency, style, and unparalleled comfort. Join the future of
+            airflow!
+          </p>
         </div>
         <div className="flex justify-center items-center">
-            <button className="bg-[#FF1F25] px-6 py-3 text-white rounded-full">Learn More</button>
-            <Button type={"up-right"}/>
+          <button className="bg-[#FF1F25] px-6 py-3 text-white rounded-full">
+            Learn More
+          </button>
+          <Button type={"up-right"} />
         </div>
       </div>
     </div>
