e327ad15eb5c5022b9d93d136b584be96d65bbac
Sreejit-Sengupto <sreesen2003@gmail.com>
2024-02-28 00:21:05 +0530

    removes uneccessary imports

--- src/utils/ClientReviewCard.jsx
+++ src/utils/ClientReviewCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ProductCard = () => {
+const ClientReviewCard = () => {
   return (
     <div className="max-w-xl max-h-80 p-6 text-center relative bg-white rounded-2xl">
       <div className="absolute w-20 h-32 top-0 left-[50%] translate-x-[-50%] translate-y-[-50%] rounded-full curve--product--card bg-white -z-10"></div>
@@ -13,4 +13,4 @@ const ProductCard = () => {
   );
 };
 
-export default ProductCard;
+export default ClientReviewCard;
