abe2a2c84dbe26c4358959877349482b8eca94b7
Sreejit-Sengupto <sreesen2003@gmail.com>
2024-03-02 22:58:11 +0530

    Removes createRoutesFromElement

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider,
+  Routes,
+  BrowserRouter as Router,
 } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./components/Home/Home";
@@ -20,29 +22,51 @@ import CategoryThree from "./components/Home/Categories/CategoryThree";
 import CategoryFour from "./components/Home/Categories/CategoryFour";
 import DirectorsMessage from "./components/Directors Message/DirectorsMessage";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Home />}>
-        {/* In order to navigate to different categories in the Categories section of the Home page, I am nesting the individual categories page inside the Home component, the Outlet will be recieved in the Categories component and when their respective Links will be clicked the user will be able to see different categories */}
-        {/* <Route index element={<CategoryOne />} />
-        <Route path="category-2" element={<CategoryTwo />} />
-        <Route path="category-3" element={<CategoryThree />} />
-        <Route path="category-4" element={<CategoryFour />} /> */}
-      </Route>
-      <Route path="/about" element={<About />} />
-      <Route path="/directors-message" element={<DirectorsMessage />} />
-      <Route path="/manufacturing" element={<Manufacturing />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/dealer" element={<Dealer />} />
-      <Route path="/Brochure" element={<Brochure />} />
-      <Route path="/contact-us" element={<ContactUs />} />
-    </Route>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout />}>
+//       <Route index element={<Home />}>
+//         {/* In order to navigate to different categories in the Categories section of the Home page, I am nesting the individual categories page inside the Home component, the Outlet will be recieved in the Categories component and when their respective Links will be clicked the user will be able to see different categories */}
+//         {/* <Route index element={<CategoryOne />} />
+//         <Route path="category-2" element={<CategoryTwo />} />
+//         <Route path="category-3" element={<CategoryThree />} />
+//         <Route path="category-4" element={<CategoryFour />} /> */}
+//       </Route>
+//       <Route path="/about" element={<About />} />
+//       <Route path="/directors-message" element={<DirectorsMessage />} />
+//       <Route path="/manufacturing" element={<Manufacturing />} />
+//       <Route path="/products" element={<Products />} />
+//       <Route path="/dealer" element={<Dealer />} />
+//       <Route path="/Brochure" element={<Brochure />} />
+//       <Route path="/contact-us" element={<ContactUs />} />
+//     </Route>
+//   )
+// );
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  // return <RouterProvider router={router} />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />}>
+            {/* In order to navigate to different categories in the Categories section of the Home page, I am nesting the individual categories page inside the Home component, the Outlet will be recieved in the Categories component and when their respective Links will be clicked the user will be able to see different categories */}
+            <Route index element={<CategoryOne />} />
+            <Route path="category-2" element={<CategoryTwo />} />
+            <Route path="category-3" element={<CategoryThree />} />
+            <Route path="category-4" element={<CategoryFour />} />
+          </Route>
+          <Route path="about" element={<About />} />
+          <Route path="directors-message" element={<DirectorsMessage />} />
+          <Route path="manufacturing" element={<Manufacturing />} />
+          <Route path="products" element={<Products />} />
+          <Route path="dealer" element={<Dealer />} />
+          <Route path="Brochure" element={<Brochure />} />
+          <Route path="contact-us" element={<ContactUs />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
